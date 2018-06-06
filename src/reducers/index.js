8da36a41a4a1f1d8c420bf6a93d84c204b384e8d
Eduardo Omine <eduardo.omine@gmail.com>
2018-06-06 18:04:23 -0300

    implementing RoadmapReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import ActivityReducer from './ActivityReducer';
 import FiltersReducer from './FiltersReducer';
+import RoadmapReducer from './RoadmapReducer';
 import SequencesReducer from './SequencesReducer';
 
 export default combineReducers({
   ActivityReducer,
   FiltersReducer,
+  RoadmapReducer,
   SequencesReducer,
 });
