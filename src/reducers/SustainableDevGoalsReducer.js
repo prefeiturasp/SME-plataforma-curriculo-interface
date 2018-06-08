e87e1d2a1fe20f4bbce0e246e6d4655d3738b775
Eduardo Omine <eduardo.omine@gmail.com>
2018-06-08 14:19:34 -0300

    added SustainableDevGoalsReducer

---
+++ src/reducers/SustainableDevGoalsReducer.js
@@ -0,0 +1,64 @@
+const initialState = {
+  items: [
+    {
+      index: 1,
+      icon: 'http://via.placeholder.com/60x40',
+      name: 'Erradicação da Pobreza',
+      text: 'Identificar transformações de energia e identificar variáveis que influem nesse fenômeno (por exemplo, ao bater na mesa, transformamos energia mecânica em energia sonora).',
+    },
+    {
+      index: 2,
+      icon: 'http://via.placeholder.com/60x40',
+      name: 'Erradicação da Pobreza',
+      text: 'Identificar transformações de energia e identificar variáveis que influem nesse fenômeno (por exemplo, ao bater na mesa, transformamos energia mecânica em energia sonora).',
+    },
+    {
+      index: 3,
+      icon: 'http://via.placeholder.com/60x40',
+      name: 'Erradicação da Pobreza',
+      text: 'Identificar transformações de energia e identificar variáveis que influem nesse fenômeno (por exemplo, ao bater na mesa, transformamos energia mecânica em energia sonora).',
+    },
+    {
+      index: 4,
+      icon: 'http://via.placeholder.com/60x40',
+      name: 'Erradicação da Pobreza',
+      text: 'Identificar transformações de energia e identificar variáveis que influem nesse fenômeno (por exemplo, ao bater na mesa, transformamos energia mecânica em energia sonora).',
+    },
+    {
+      index: 5,
+      icon: 'http://via.placeholder.com/60x40',
+      name: 'Erradicação da Pobreza',
+      text: 'Identificar transformações de energia e identificar variáveis que influem nesse fenômeno (por exemplo, ao bater na mesa, transformamos energia mecânica em energia sonora).',
+    },
+    {
+      index: 6,
+      icon: 'http://via.placeholder.com/60x40',
+      name: 'Erradicação da Pobreza',
+      text: 'Identificar transformações de energia e identificar variáveis que influem nesse fenômeno (por exemplo, ao bater na mesa, transformamos energia mecânica em energia sonora).',
+    },
+    {
+      index: 7,
+      icon: 'http://via.placeholder.com/60x40',
+      name: 'Erradicação da Pobreza',
+      text: 'Identificar transformações de energia e identificar variáveis que influem nesse fenômeno (por exemplo, ao bater na mesa, transformamos energia mecânica em energia sonora).',
+    },
+    {
+      index: 8,
+      icon: 'http://via.placeholder.com/60x40',
+      name: 'Erradicação da Pobreza',
+      text: 'Identificar transformações de energia e identificar variáveis que influem nesse fenômeno (por exemplo, ao bater na mesa, transformamos energia mecânica em energia sonora).',
+    },
+    {
+      index: 9,
+      icon: 'http://via.placeholder.com/60x40',
+      name: 'Erradicação da Pobreza',
+      text: 'Identificar transformações de energia e identificar variáveis que influem nesse fenômeno (por exemplo, ao bater na mesa, transformamos energia mecânica em energia sonora).',
+    },
+  ],
+};
+
+function SustainableDevGoalsReducer(state = initialState, action) {
+  return state;
+}
+
+export default SustainableDevGoalsReducer;
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,10 +3,12 @@ import ActivityReducer from './ActivityReducer';
 import FiltersReducer from './FiltersReducer';
 import RoadmapReducer from './RoadmapReducer';
 import SequencesReducer from './SequencesReducer';
+import SustainableDevGoalsReducer from './SustainableDevGoalsReducer';
 
 export default combineReducers({
   ActivityReducer,
   FiltersReducer,
   RoadmapReducer,
   SequencesReducer,
+  SustainableDevGoalsReducer,
 });
