5760cdb4906fa12eb3b91fa0be963d97dd77b3d2
Eduardo Omine <eduardo.omine@gmail.com>
2018-06-08 15:32:56 -0300

    added KnowledgeMatrixReducer

---
+++ src/reducers/KnowledgeMatrixReducer.js
@@ -0,0 +1,46 @@
+const initialState = {
+  items: [
+    {
+      id: 1,
+      index: 1,
+      name: 'Pensamento Científico, Crítico e Criativo',
+      text: 'Identificar transformações de energia e identificar variáveis que influem nesse fenômeno (por exemplo, ao bater na mesa, transformamos energia mecânica em energia sonora).',
+    },
+    {
+      id: 2,
+      index: 2,
+      name: 'Resolução de Problemas',
+      text: 'Identificar transformações de energia e identificar variáveis que influem nesse fenômeno (por exemplo, ao bater na mesa, transformamos energia mecânica em energia sonora).',
+    },
+    {
+      id: 3,
+      index: 3,
+      name: 'Comunicação',
+      text: 'Identificar transformações de energia e identificar variáveis que influem nesse fenômeno (por exemplo, ao bater na mesa, transformamos energia mecânica em energia sonora).',
+    },
+    {
+      id: 4,
+      index: 4,
+      name: 'Autoconhecimento e Autocuidado',
+      text: 'Identificar transformações de energia e identificar variáveis que influem nesse fenômeno (por exemplo, ao bater na mesa, transformamos energia mecânica em energia sonora).',
+    },
+    {
+      id: 5,
+      index: 5,
+      name: 'Autonomia e Determinação',
+      text: 'Identificar transformações de energia e identificar variáveis que influem nesse fenômeno (por exemplo, ao bater na mesa, transformamos energia mecânica em energia sonora).',
+    },
+    {
+      id: 6,
+      index: 6,
+      name: 'Abertura à Diversidade',
+      text: 'Identificar transformações de energia e identificar variáveis que influem nesse fenômeno (por exemplo, ao bater na mesa, transformamos energia mecânica em energia sonora).',
+    },
+  ],
+};
+
+function KnowledgeMatrixReducer(state = initialState, action) {
+  return state;
+}
+
+export default KnowledgeMatrixReducer;
