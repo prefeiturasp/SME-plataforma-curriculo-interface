078600b7a5c130779d5d3ae9aaba882b8c91d8f1
Eduardo Omine <eduardo.omine@gmail.com>
2018-06-11 14:52:44 -0300

    implementing knowledge matrix item page

--- src/views/knowledgeMatrix/KnowledgeMatrixItem.js
+++ src/views/knowledgeMatrix/KnowledgeMatrixItem.js
@@ -1,13 +1,47 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { NavLink } from 'react-router-dom';
+import { connect } from 'react-redux';
+import iconCloseBig from '../../images/iconCloseBig.svg';
+import styles from './KnowledgeMatrixItem.css';
 
 class KnowledgeMatrixItem extends Component {
   render() {
     return (
-      <section>
-        <h1>Matriz de Saberes (Item)</h1>
+      <section className={styles.wrapper}>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8 offset-md-2">
+              <div className={styles.number}>
+                {this.props.data.index}
+              </div>
+              <h1>{this.props.data.name}</h1>
+              <h2>Saber</h2>
+              <p>{this.props.data.know}</p>
+              <h2>Para</h2>
+              <p>{this.props.data.to}</p>
+              <NavLink to='/sequencias' className={styles.button}>
+                Ver Sequências de Atividades Relacionadas
+              </NavLink>
+              <NavLink to='/matriz-de-saberes' className={styles.close}>
+                <img src={iconCloseBig} alt='Fechar' />
+              </NavLink>
+            </div>
+          </div>
+        </div>
       </section>
     );
   }
 }
 
-export default KnowledgeMatrixItem;
+KnowledgeMatrixItem.propTypes = {
+  data: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = state => {
+  return {
+    data: state.KnowledgeMatrixReducer.currItem,
+  };
+};
+
+export default connect(mapStateToProps)(KnowledgeMatrixItem);
