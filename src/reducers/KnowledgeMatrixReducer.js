b20be85230c056babf083b2d620e7df0787007b7
Eduardo Omine <eduardo.omine@gmail.com>
2018-06-11 14:52:28 -0300

    adjusted reducer

--- src/reducers/KnowledgeMatrixReducer.js
+++ src/reducers/KnowledgeMatrixReducer.js
@@ -4,40 +4,49 @@ const initialState = {
       id: 1,
       index: 1,
       name: 'Pensamento Científico, Crítico e Criativo',
-      text: 'Identificar transformações de energia e identificar variáveis que influem nesse fenômeno (por exemplo, ao bater na mesa, transformamos energia mecânica em energia sonora).',
+      know: 'Acessar, selecionar e organizar o conhecimento com curiosidade, pensamento científico, criticidade e criatividade;',
+      to: 'Observar, questionar, investigar causas, elaborar e testar hipóteses; refletir, interpretar e analisar ideias e fatos em profundidade; produzir e utilizar evidências.',
     },
     {
       id: 2,
       index: 2,
       name: 'Resolução de Problemas',
-      text: 'Identificar transformações de energia e identificar variáveis que influem nesse fenômeno (por exemplo, ao bater na mesa, transformamos energia mecânica em energia sonora).',
+      know: 'Acessar, selecionar e organizar o conhecimento com curiosidade, pensamento científico, criticidade e criatividade;',
+      to: 'Observar, questionar, investigar causas, elaborar e testar hipóteses; refletir, interpretar e analisar ideias e fatos em profundidade; produzir e utilizar evidências.',
     },
     {
       id: 3,
       index: 3,
       name: 'Comunicação',
-      text: 'Identificar transformações de energia e identificar variáveis que influem nesse fenômeno (por exemplo, ao bater na mesa, transformamos energia mecânica em energia sonora).',
+      know: 'Acessar, selecionar e organizar o conhecimento com curiosidade, pensamento científico, criticidade e criatividade;',
+      to: 'Observar, questionar, investigar causas, elaborar e testar hipóteses; refletir, interpretar e analisar ideias e fatos em profundidade; produzir e utilizar evidências.',
     },
     {
       id: 4,
       index: 4,
       name: 'Autoconhecimento e Autocuidado',
-      text: 'Identificar transformações de energia e identificar variáveis que influem nesse fenômeno (por exemplo, ao bater na mesa, transformamos energia mecânica em energia sonora).',
+      know: 'Acessar, selecionar e organizar o conhecimento com curiosidade, pensamento científico, criticidade e criatividade;',
+      to: 'Observar, questionar, investigar causas, elaborar e testar hipóteses; refletir, interpretar e analisar ideias e fatos em profundidade; produzir e utilizar evidências.',
     },
     {
       id: 5,
       index: 5,
       name: 'Autonomia e Determinação',
-      text: 'Identificar transformações de energia e identificar variáveis que influem nesse fenômeno (por exemplo, ao bater na mesa, transformamos energia mecânica em energia sonora).',
+      know: 'Acessar, selecionar e organizar o conhecimento com curiosidade, pensamento científico, criticidade e criatividade;',
+      to: 'Observar, questionar, investigar causas, elaborar e testar hipóteses; refletir, interpretar e analisar ideias e fatos em profundidade; produzir e utilizar evidências.',
     },
     {
       id: 6,
       index: 6,
       name: 'Abertura à Diversidade',
-      text: 'Identificar transformações de energia e identificar variáveis que influem nesse fenômeno (por exemplo, ao bater na mesa, transformamos energia mecânica em energia sonora).',
+      know: 'Acessar, selecionar e organizar o conhecimento com curiosidade, pensamento científico, criticidade e criatividade;',
+      to: 'Observar, questionar, investigar causas, elaborar e testar hipóteses; refletir, interpretar e analisar ideias e fatos em profundidade; produzir e utilizar evidências.',
     },
   ],
+  currItem: null,
 };
+
+initialState.currItem = initialState.items[0];
 
 function KnowledgeMatrixReducer(state = initialState, action) {
   return state;
