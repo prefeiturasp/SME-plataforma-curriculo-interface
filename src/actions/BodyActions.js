7a31698a8e5177b30bca9a85e3718595b10a594f
Eduardo Omine <eduardo.omine@gmail.com>
2018-06-14 14:30:27 -0300

    updated body reducer for home

--- src/actions/BodyActions.js
+++ src/actions/BodyActions.js
@@ -1,7 +1,15 @@
 const BodyActions = {
+  ENTER_HOME: 'ENTER_HOME',
+  EXIT_HOME: 'EXIT_HOME',
   HIDE_POPUP: 'HIDE_POPUP',
   SHOW_POPUP: 'SHOW_POPUP',
   
+  enterHome() {
+    return { type: BodyActions.ENTER_HOME };
+  },
+  exitHome() {
+    return { type: BodyActions.EXIT_HOME };
+  },
   hidePopup() {
     return { type: BodyActions.HIDE_POPUP };
   },
--- src/reducers/BodyReducer.js
+++ src/reducers/BodyReducer.js
@@ -1,14 +1,23 @@
 import BodyActions from '../actions/BodyActions';
 
-const initialState = { hasPopup: false };
+const initialState = {
+  hasPopup: false,
+  isHome: false,
+};
 
 function BodyReducer(state = initialState, action) {
   switch (action.type) {
+    case BodyActions.ENTER_HOME:
+      return Object.assign({}, state, { isHome: true });
+
+    case BodyActions.EXIT_HOME:
+      return Object.assign({}, state, { isHome: false });
+
     case BodyActions.HIDE_POPUP:
-      return { hasPopup: false };
+      return Object.assign({}, state, { hasPopup: false });
 
     case BodyActions.SHOW_POPUP:
-      return { hasPopup: true };
+      return Object.assign({}, state, { hasPopup: true });
 
     default:
       return initialState;
