bb7dc480e9cfe03d7d70500cea72aaee163fbbf9
Eduardo Omine <eduardo.omine@gmail.com>
2018-06-20 17:54:28 -0300

    implementing integration with roadmap endpoint

--- src/reducers/RoadmapReducer.js
+++ src/reducers/RoadmapReducer.js
@@ -1,37 +1,23 @@
 import RoadpmapActions from '../actions/RoadmapActions';
 
 const initialState = {
-  items: [
-    // {
-    //   title: 'Conheça e explore o Currículo da Cidade.',
-    //   text: 'Identificar transformações de energia e identificar variáveis que influem nesse fenômeno (por exemplo, ao bater na mesa, transformamos energia mecânica em energia sonora).',
-    //   isPublished: true,
-    // },
-    // {
-    //   title: 'Conheça e explore o Currículo da Cidade.',
-    //   text: 'Identificar transformações de energia e identificar variáveis que influem nesse fenômeno (por exemplo, ao bater na mesa, transformamos energia mecânica em energia sonora).',
-    //   isPublished: true,
-    // },
-    // {
-    //   title: 'Conheça e explore o Currículo da Cidade.',
-    //   text: 'Identificar transformações de energia e identificar variáveis que influem nesse fenômeno (por exemplo, ao bater na mesa, transformamos energia mecânica em energia sonora).',
-    //   isPublished: false,
-    // },
-    // {
-    //   title: 'Conheça e explore o Currículo da Cidade.',
-    //   text: 'Identificar transformações de energia e identificar variáveis que influem nesse fenômeno (por exemplo, ao bater na mesa, transformamos energia mecânica em energia sonora).',
-    //   isPublished: false,
-    // },
-  ],
+  items: [],
 };
 
 function RoadmapReducer(state = initialState, action) {
   switch (action.type) {
     case RoadpmapActions.LOAD:
-      return { ...state, isLoading: true };
+      return {
+        ...state,
+        isLoading: true
+      };
 
     case RoadpmapActions.LOADED:
-      return { ...state, isLoading: false, items: action.data };
+      return {
+        ...state,
+        isLoading: false,
+        items: action.data
+      };
 
     default:
       return state;
