e40d11b6cdfb1b1f4fdf319b1857cf48820c610d
Eduardo Omine <eduardo.omine@gmail.com>
2018-07-04 20:08:05 -0300

    implemented items with links

--- src/views/common/KnowledgeMatrixItem.js
+++ src/views/common/KnowledgeMatrixItem.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { NavLink } from 'react-router-dom';
 import styles from './KnowledgeMatrixItem.css';
 
 class KnowledgeMatrixItem extends Component {
   render() {
-    return (
+    const link = `/matriz-de-saberes/${this.props.data.sequence}`;
+
+    return this.props.isLink ? (
+      <li className={styles.wrapper}>
+        <NavLink to={link}>
+          <div>{this.props.data.sequence}</div>
+          <div>{this.props.data.title}</div>
+        </NavLink>
+      </li>
+    ) : (
       <li className={styles.wrapper}>
         <div>{this.props.data.sequence}</div>
         <div>{this.props.data.title}</div>
@@ -15,6 +25,7 @@ class KnowledgeMatrixItem extends Component {
 
 KnowledgeMatrixItem.propTypes = {
   data: PropTypes.object.isRequired,
+  isLink: PropTypes.bool,
 };
 
 export default KnowledgeMatrixItem;
--- src/views/common/SustainableDevGoalItem.js
+++ src/views/common/SustainableDevGoalItem.js
@@ -1,21 +1,36 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { NavLink } from 'react-router-dom';
 import { API_URL } from '../../constants';
 import styles from './SustainableDevGoalItem.css';
 
 class SustainableDevGoalItem extends Component {
   render() {
-    return (
+    const image = (
       <img
         src={API_URL + this.props.data.icon_url}
-        alt={this.props.data.name}
-        className={styles.wrapper} />
+        alt={this.props.data.name} />
+    );
+
+    const link = `/ods/${this.props.data.id}`;
+
+    return this.props.isLink ? (
+      <li className={styles.wrapper}>
+        <NavLink to={link}>
+          {image}
+        </NavLink>
+      </li>
+    ) : (
+      <li className={styles.wrapper}>
+        {image}
+      </li>
     );
   }
 }
 
 SustainableDevGoalItem.propTypes = {
   data: PropTypes.object.isRequired,
+  isLink: PropTypes.bool,
 };
 
 export default SustainableDevGoalItem;
