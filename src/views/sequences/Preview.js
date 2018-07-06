67ff72ba70ef155aa7d2ad1ed7dae649fad5740c
Eduardo Omine <eduardo.omine@gmail.com>
2018-07-06 14:09:25 -0300

    fixed LearningObjectiveItem markup and css

--- src/views/sequences/Preview.js
+++ src/views/sequences/Preview.js
@@ -24,7 +24,9 @@ class Preview extends Component {
 
     const learningObjectives = this.props.data.learning_objectives.map((item, i) => {
       return (
-        <LearningObjectiveItem key={i} data={item} />
+        <li>
+          <LearningObjectiveItem key={i} data={item} />
+        </li>
       );
     });
 
