7defd51a4064807f8ec486583bf077f49b41a621
Eduardo Omine <eduardo.omine@gmail.com>
2018-07-11 18:36:49 -0300

    added ResultsLoading

---
+++ src/views/sequences/ResultsLoading.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Loading from '../util/Loading';
+import styles from './ResultsLoading.css';
+
+class ResultsLoading extends Component {
+  render() {
+    const style = { height: this.props.height };
+    return (
+      <div className="container">
+        <div className={styles.wrapper} style={style}>
+          <Loading />
+        </div>
+      </div>
+    );
+  }
+}
+
+ResultsLoading.propTypes = {
+  height: PropTypes.number.isRequired,
+};
+
+export default ResultsLoading;
