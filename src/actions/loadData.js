bc52d73e11bdf826bf2212a1d137994df5c44a5d
Eduardo Omine <eduardo.omine@gmail.com>
2018-07-18 15:56:44 -0300

    implemented reading pagination headers

--- src/actions/loadData.js
+++ src/actions/loadData.js
@@ -1,14 +1,30 @@
+import parse from 'parse-link-header';
 import BodyActions from './BodyActions';
 import { API_URL } from '../constants';
+
+function getFullUrl(url) {
+  return url.match(/http/) ? url : API_URL + url;
+}
+
+function getNextPage(headers) {
+  if (headers.has('link')) {
+    const parsed = parse(headers.get('link'));
+    return parsed.next ? parsed.next.url : null;
+  }
+
+  return null;
+}
 
 export default function loadData(url, action1, action2) {
   return dispatch => {
     dispatch({ type: action1 });
-    fetch(API_URL + url)
-      .then(response => response.json())
-      .then(data => {
-        dispatch({ type: BodyActions.HIDE_LOADING });
-        dispatch({ data, type: action2 });
+    fetch(getFullUrl(url))
+      .then(response => {
+        response.json().then(data => {
+          const nextPage = getNextPage(response.headers);
+          dispatch({ type: BodyActions.HIDE_LOADING });
+          dispatch({ data, nextPage, type: action2 });
+        })
       });
   };
 }
