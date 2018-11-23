b47a9458fad86d0debed8b03f03cebe6a77262f7
Eduardo Omine <eduardo.omine@gmail.com>
2018-11-23 18:32:39 -0200

    updated GenericItem layout

--- src/views/common/GenericItem.js
+++ src/views/common/GenericItem.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import styles from './GenericItem.css';
+import styles from './GenericItem.scss';
 
 class GenericItem extends Component {
   render() {
