b0c0322c88107be8509cb2e802157b47057612a9
Eduardo Omine <eduardo.omine@gmail.com>
2018-11-27 19:11:28 -0200

    separated MobileMenu component from Header

--- src/views/header/Header.js
+++ src/views/header/Header.js
@@ -4,127 +4,60 @@ import Headroom from 'react-headroom';
 import { NavLink } from 'react-router-dom';
 import { connect } from 'react-redux';
 import BodyActions from '../../actions/BodyActions';
-import getWindowWidth from '../util/getWindowWidth';
-import iconCloseBig from '../../images/iconCloseBig.svg';
-import iconMenu from '../../images/iconMenu.svg';
-import iconMenuWhite from '../../images/iconMenuWhite.svg';
-import logoColor from '../../images/logo.svg';
-import logoWhite from '../../images/logoWhite.svg';
 import styles from './Header.scss';
 
 class Header extends Component {
-  constructor(props) {
-    super(props);
-    this.onClickedMenu = this.onClickedMenu.bind(this);
-    this.onClickedClose = this.onClickedClose.bind(this);
-    this.state = { isMenuExpanded: false };
-  }
-
-  onClickedMenu() {
-    this.setState({ isMenuExpanded: true });
-
-    if (getWindowWidth() < 768) {
-      this.props.showPopup();
-    }
-  }
-
-  onClickedClose() {
-    this.setState({ isMenuExpanded: false });
-    this.props.hidePopup();
+  onClickedToggler = () => {
+    this.props.showMobileMenu();
   }
   
   render() {
-    const classes = this.state.isMenuExpanded ? [styles.wrapper, styles.isMenuExpanded] : [styles.wrapper];
-
-    const icon = this.props.isHome && !this.props.hasScrolled ? iconMenuWhite : iconMenu;
-    const logo = this.props.isHome && !this.props.hasScrolled ? logoWhite : logoColor;
-
     const data = [
       {
         to: '/',
         label: 'Home',
-        isSub: false,
       },
       {
         to: '/sequencias',
         label: 'Sequências de Atividades',
-        isSub: false,
       },
       {
         to: '/curriculo',
         label: 'Currículo da Cidade',
-        isSub: false,
-      },
-      {
-        to: '/ods',
-        label: 'Objetivos de Desenvolvimento Sustentável',
-        isSub: true,
-      },
-      {
-        to: '/matriz-de-saberes',
-        label: 'Matriz de Saberes',
-        isSub: true,
-      },
-      {
-        to: '/objetivos-de-aprendizagem',
-        label: 'Objetivos de Aprendizagem',
-        isSub: true,
       },
       {
         to: '/descobrir',
         label: 'O que vem por aí',
-        isSub: false,
-      },
-      {
-        div: true,
-      },
-      {
-        to: '/perfil',
-        label: 'Meu perfil',
-        isSub: false,
-      },
-      {
-        to: '/sair',
-        label: 'Sair',
-        isSub: false,
       },
     ];
 
     const links = data.map((item, i) => {
-      if (item.div) {
-        return <hr key={i} />;
-      } else {
-        const klass = item.isSub ? styles.sub : null;
-        return (
-          <NavLink
-            key={i}
-            to={item.to}
-            className={klass}
-            onClick={this.onClickedClose}>
-            {item.label}
-          </NavLink>
-        );
-      }
+      return (
+        <NavLink
+          key={i}
+          to={item.to}
+          onClick={this.onClickedClose}>
+          {item.label}
+        </NavLink>
+      );
     })
 
     return (
-      <Headroom disableInlineStyles>
-        <header className={classes.join(' ')}>
+      <Headroom disableInlineStyles downTolerance={70}>
+        <header className={styles.wrapper}>
           <NavLink to="/">
             <div className={styles.logo}>
-              <img src={logo} alt="Currículo Digital da Cidade de São Paulo" />
+              <span className={styles.logoImage} />
               <h1>Currículo Digital da Cidade de São Paulo</h1>
             </div>
           </NavLink>
           <nav className={styles.menu}>
             {links}
-            <button className={styles.close} onClick={this.onClickedClose.bind(this)}>
-              <img src={iconCloseBig} alt="Fechar" />
-            </button>
           </nav>
-          <button className={styles.toggler} onClick={this.onClickedMenu.bind(this)}>
-            <img src={icon} alt="Menu" />
-          </button>
+          <button
+            className={styles.toggler}
+            onClick={this.onClickedToggler}
+          />
         </header>
       </Headroom>
     );
@@ -132,28 +65,15 @@ class Header extends Component {
 }
 
 Header.propTypes = {
-  hasScrolled: PropTypes.bool.isRequired,
-  isHome: PropTypes.bool.isRequired,
-  hidePopup: PropTypes.func.isRequired,
-  showPopup: PropTypes.func.isRequired,
-};
-
-const mapStateToProps = state => {
-  return {
-    hasScrolled: state.BodyReducer.hasScrolled,
-    isHome: state.BodyReducer.isHome,
-  };
+  showMobileMenu: PropTypes.func.isRequired,
 };
 
 const mapDispatchToProps = dispatch => {
   return {
-    hidePopup: () => {
-      dispatch(BodyActions.hidePopup());
-    },
-    showPopup: () => {
-      dispatch(BodyActions.showPopup());
+    showMobileMenu: () => {
+      dispatch(BodyActions.showMobileMenu());
     },
   };
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(Header);
+export default connect(null, mapDispatchToProps)(Header);
---
+++ src/views/mobileMenu/MobileMenu.js
@@ -0,0 +1,115 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { NavLink } from 'react-router-dom';
+import { connect } from 'react-redux';
+import BodyActions from '../../actions/BodyActions';
+import iconCloseBig from '../../images/iconCloseBig.svg';
+import styles from './MobileMenu.scss';
+
+class MobileMenu extends Component {
+  onClickedClose = () => {
+    this.props.hideMobileMenu();
+  }
+  
+  render() {
+    const classes = this.props.hasMobileMenu ? [styles.wrapper, styles.isExpanded] : [styles.wrapper];
+
+    const data = [
+      {
+        to: '/',
+        label: 'Home',
+        isSub: false,
+      },
+      {
+        to: '/sequencias',
+        label: 'Sequências de Atividades',
+        isSub: false,
+      },
+      {
+        to: '/curriculo',
+        label: 'Currículo da Cidade',
+        isSub: false,
+      },
+      {
+        to: '/ods',
+        label: 'Objetivos de Desenvolvimento Sustentável',
+        isSub: true,
+      },
+      {
+        to: '/matriz-de-saberes',
+        label: 'Matriz de Saberes',
+        isSub: true,
+      },
+      {
+        to: '/objetivos-de-aprendizagem',
+        label: 'Objetivos de Aprendizagem',
+        isSub: true,
+      },
+      {
+        to: '/descobrir',
+        label: 'O que vem por aí',
+        isSub: false,
+      },
+      {
+        div: true,
+      },
+      {
+        to: '/perfil',
+        label: 'Meu perfil',
+        isSub: false,
+      },
+      {
+        to: '/sair',
+        label: 'Sair',
+        isSub: false,
+      },
+    ];
+
+    const links = data.map((item, i) => {
+      if (item.div) {
+        return <hr key={i} />;
+      } else {
+        const klass = item.isSub ? styles.sub : null;
+        return (
+          <NavLink
+            key={i}
+            to={item.to}
+            className={klass}
+            onClick={this.onClickedClose}>
+            {item.label}
+          </NavLink>
+        );
+      }
+    })
+
+    return (
+      <nav className={classes.join(' ')}>
+        {links}
+        <button className={styles.close} onClick={this.onClickedClose.bind(this)}>
+          <img src={iconCloseBig} alt="Fechar" />
+        </button>
+      </nav>
+    );
+  }
+}
+
+MobileMenu.propTypes = {
+  hasMobileMenu: PropTypes.bool,
+  hideMobileMenu: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = state => {
+  return {
+    hasMobileMenu: state.BodyReducer.hasMobileMenu,
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    hideMobileMenu: () => {
+      dispatch(BodyActions.hideMobileMenu());
+    },
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(MobileMenu);
