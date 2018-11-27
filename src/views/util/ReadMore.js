f595801b40c50b101009e5fa791b63690c79dd9f
Eduardo Omine <eduardo.omine@gmail.com>
2018-11-27 16:51:49 -0200

    fixed js warning and code formatting

--- src/views/util/ReadMore.js
+++ src/views/util/ReadMore.js
@@ -4,78 +4,78 @@ import Truncate from 'react-truncate';
 import styles from './ReadMore.scss';
 
 class ReadMore extends Component {
-    constructor(...args) {
-        super(...args);
+  constructor(...args) {
+    super(...args);
 
-        this.state = {
-            expanded: false,
-            truncated: false
-        };
+    this.state = {
+      expanded: false,
+      truncated: false,
+    };
 
-        this.handleTruncate = this.handleTruncate.bind(this);
-        this.toggleLines = this.toggleLines.bind(this);
+    this.handleTruncate = this.handleTruncate.bind(this);
+    this.toggleLines = this.toggleLines.bind(this);
+  }
+
+  handleTruncate(truncated) {
+    if (this.state.truncated !== truncated) {
+      this.setState({
+        truncated,
+      });
     }
+  }
 
-    handleTruncate(truncated) {
-        if (this.state.truncated !== truncated) {
-            this.setState({
-                truncated
-            });
-        }
-    }
+  toggleLines(e) {
+    e.preventDefault();
 
-    toggleLines(event) {
-        event.preventDefault();
+    this.setState({
+      expanded: !this.state.expanded,
+    });
+  }
 
-        this.setState({
-            expanded: !this.state.expanded
-        });
-    }
+  render() {
+    const { children, more, less, lines } = this.props;
+    const { expanded, truncated } = this.state;
 
-    render() {
-        const {
-            children,
-            more,
-            less,
-            lines
-        } = this.props;
-
-        const {
-            expanded,
-            truncated
-        } = this.state;
-
-        return (
-            <div>
-                <Truncate
-                    lines={!expanded && lines}
-                    ellipsis={(
-                        <span>...<br/><a href='#' className={styles.toggler} onClick={this.toggleLines}>{more}</a></span>
-                    )}
-                    onTruncate={this.handleTruncate}
-                >
-                <div
-                  dangerouslySetInnerHTML={{__html: children}} />
-                </Truncate>
-                {!truncated && expanded && (
-                    <span><a href='#' className={styles.toggler} onClick={this.toggleLines}>{less}</a></span>
-                )}
-            </div>
-        );
-    }
+    return (
+      <div>
+        <Truncate
+          lines={!expanded && lines}
+          ellipsis={(
+            <span>
+              ...
+              <br/>
+              <button className={styles.toggler} onClick={this.toggleLines}>
+                {more}
+              </button>
+            </span>
+          )}
+          onTruncate={this.handleTruncate}
+        >
+        <div dangerouslySetInnerHTML={{__html: children}} />
+        </Truncate>
+        {!truncated && expanded && (
+          <span>
+            <button className={styles.toggler} onClick={this.toggleLines}>
+              {less}
+            </button>
+          </span>
+        )}
+      </div>
+    );
+  }
 }
 
 ReadMore.defaultProps = {
-    lines: 3,
-    more: 'Mostrar mais',
-    less: 'Mostrar menos'
+  lines: 3,
+  more: 'Mostrar mais',
+  less: 'Mostrar menos'
 };
 
 ReadMore.propTypes = {
-    children: PropTypes.string.isRequired,
-    lines: PropTypes.number,
-    less: PropTypes.string,
-    more: PropTypes.string
+  children: PropTypes.string.isRequired,
+  lines: PropTypes.number,
+  less: PropTypes.string,
+  more: PropTypes.string
 };
 
 export default ReadMore;
