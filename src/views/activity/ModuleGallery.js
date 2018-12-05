38a58e7dbf62796721c9ee2333a82aaab5b913c7
Eduardo Omine <eduardo.omine@gmail.com>
2018-12-05 11:38:59 -0200

    implementing Activity ModuleGallery

--- src/views/activity/ModuleGallery.js
+++ src/views/activity/ModuleGallery.js
@@ -1,14 +1,58 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import ImageGallery from 'react-image-gallery';
 import styles from './ModuleGallery.scss';
 
 class ModuleGallery extends Component {
+  renderLeftNav = (onClick, disabled) => {
+    return (
+      <button
+        className={styles.btnLeft}
+        disabled={disabled}
+        onClick={onClick}
+      />
+    );
+  }
+
+  renderRightNav = (onClick, disabled) => {
+    return (
+      <button
+        className={styles.btnRight}
+        disabled={disabled}
+        onClick={onClick}
+      />
+    );
+  }
+
+  renderFullscreenButton = (onClick, isFullscreen) => {
+    return (
+      <button
+        className={styles.btnFull}
+        onClick={onClick}
+      />
+    );
+  }
+
   render() {
     return (
       <div className={styles.wrapper}>
+        <ImageGallery
+          items={this.props.images}
+          renderLeftNav={this.renderLeftNav}
+          renderRightNav={this.renderRightNav}
+          renderFullscreenButton={this.renderFullscreenButton}
+          showPlayButton={false}
+          showThumbnails={false}
+          slideDuration={300}
+          useBrowserFullscreen={false}
+        />
       </div>
     );
   }
 }
+
+ModuleGallery.propTypes = {
+  images: PropTypes.array,
+};
 
 export default ModuleGallery;
