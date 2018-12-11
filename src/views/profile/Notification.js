19f38a69c0e6812380e2fc7990c58c220e2cce54
Eduardo Omine <eduardo.omine@gmail.com>
2018-12-11 15:49:11 -0200

    implemented Notification hiding

--- src/views/profile/Notification.js
+++ src/views/profile/Notification.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
+import AnimateHeight from 'react-animate-height';
 import styles from './Notification.scss';
 
 class Notification extends Component {
-  onClickedNo = () => {
+  state = {
+    isExpanded: true,
+  };
 
+  onClickedNo = () => {
+    this.setState({
+      isExpanded: false,
+    });
   }
 
   onClickedYes = () => {
@@ -11,18 +18,22 @@ class Notification extends Component {
   }
 
   render() {
+    const height = this.state.isExpanded ? 'auto' : 0;
+
     return (
-      <div className={styles.wrapper}>
-        <p>Você completou 3 sequências de atividades recentemente. Avalie agora e nos ajude a construir novos conteúdos.</p>
-        <div className={styles.buttons}>
-          <button onClick={this.onClickedNo}>
-            Agora não
-          </button>
-          <button onClick={this.onClickedYes}>
-            Avaliar sequência
-          </button>
+      <AnimateHeight height={height}>
+        <div className={styles.wrapper}>
+          <p>Você completou 3 sequências de atividades recentemente. Avalie agora e nos ajude a construir novos conteúdos.</p>
+          <div className={styles.buttons}>
+            <button onClick={this.onClickedNo}>
+              Agora não
+            </button>
+            <button onClick={this.onClickedYes}>
+              Avaliar sequência
+            </button>
+          </div>
         </div>
-      </div>
+      </AnimateHeight>
     );
   }
 }
