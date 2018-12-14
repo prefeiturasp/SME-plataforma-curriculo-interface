000e7c6ce15613803c4768c0dccfe059305e1ff9
Eduardo Omine <eduardo.omine@gmail.com>
2018-12-14 16:12:54 -0200

    removed print and characteristics routes

--- src/index.js
+++ src/index.js
@@ -14,8 +14,6 @@ import { applyMiddleware, createStore } from 'redux';
 import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
 
 import Activity from 'views/activity/Activity';
-import ActivityChars from 'views/activity/ActivityChars';
-import ActivityPrint from 'views/activity/ActivityPrint';
 import Alert from 'components/Alert';
 import AppLoading from 'components/loading/AppLoading';
 import Classrooms from 'views/profile/classrooms/Classrooms';
@@ -32,8 +30,6 @@ import Profile from 'views/profile/Profile';
 import Roadmap from 'views/roadmap/Roadmap';
 import ScrollToTop from 'components/ScrollToTop';
 import Sequence from 'views/sequence/Sequence';
-import SequenceChars from 'views/sequence/SequenceChars';
-import SequencePrint from 'views/sequence/SequencePrint';
 import Sequences from 'views/sequences/Sequences';
 import SustainableDevGoal from 'views/sustainableDevGoals/SustainableDevGoal';
 import SustainableDevGoals from 'views/sustainableDevGoals/SustainableDevGoals';
@@ -78,11 +74,7 @@ class ModalSwitch extends Component {
           <Route exact path='/sequencias/matriz-de-saberes/:mds' component={Sequences} />
           <Route exact path='/sequencias/objetivos-de-aprendizagem/:oda' component={Sequences} />
           <Route exact path='/sequencia/:slug' component={Sequence} />
-          <Route exact path='/sequencia/:slug/caracteristicas' component={SequenceChars} />
           <Route exact path='/sequencia/:slug1/atividade/:slug2' component={Activity} />
-          <Route exact path='/sequencia/:slug1/atividade/:slug2/caracteristicas' component={ActivityChars} />
-          <Route exact path='/imprimir/sequencia/:slug' component={SequencePrint} />
-          <Route exact path='/imprimir/sequencia/:slug1/atividade/:slug2' component={ActivityPrint} />
           <Route exact path='/curriculo' component={Curriculum} />
           <Route exact path='/ods' component={SustainableDevGoals} />
           <Route exact path='/matriz-de-saberes' component={KnowledgeMatrix} />
