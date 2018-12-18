4d40f0d29f51a76c35b40556396b4e0515b427a6
Eduardo Omine <eduardo.omine@gmail.com>
2018-12-18 15:31:18 -0200

    implemented Page so that Footer is always at the bottom

--- src/components/layout/Page.js
+++ src/components/layout/Page.js
@@ -2,16 +2,17 @@ import React, { Fragment } from 'react';
 import Footer from 'components/footer/Footer';
 import Header from 'components/header/Header';
 import MobileMenu from 'components/MobileMenu';
+import styles from './Page.scss';
 
 class Page extends React.PureComponent {
   render() {
     return (
-      <Fragment>
+      <div className={styles.wrapper}>
         <Header />
         <MobileMenu />
         {this.props.children}
         <Footer />
-      </Fragment>
+      </div>
     );
   }
 }
