c476a740a0202daf710f30b928bc44e7ef1d7614
Eduardo Omine <eduardo.omine@gmail.com>
2019-01-03 17:11:40 -0200

    implemented QueryField

--- src/views/sequences/filters/FilterBar.js
+++ src/views/sequences/filters/FilterBar.js
@@ -5,6 +5,7 @@ import { disableBodyScroll, clearAllBodyScrollLocks } from 'body-scroll-lock';
 import ActiveItem from './ActiveItem';
 import FiltersActions from 'actions/FiltersActions';
 import OrderButton from './OrderButton';
+import QueryField from './QueryField';
 import getWindowWidth from 'utils/getWindowWidth';
 import iconFilters from 'images/icons/filters.svg';
 import styles from './FilterBar.scss';
@@ -28,14 +29,21 @@ class FilterBar extends Component {
 
   render() {
     const filters = this.props.filters.concat(this.props.filtersExtra);
+
     const items = filters.map((item, i) => {
       return <ActiveItem key={i} data={item} />;
     });
 
+    const spacer = items.length ? <div className={styles.spacer} /> : null;
+
     return (
       <div className={styles.wrapper}>
-        <div className={styles.list}>{items}</div>
-        <div className={styles.buttons}>
+        <div>
+          <QueryField />
+          {spacer}
+          <div className={styles.list}>{items}</div>
+        </div>
+        <div className={styles.row}>
           <button className={styles.button} onClick={this.onClickedToggle}>
             <img src={iconFilters} alt="Filtros" />
             Filtros
