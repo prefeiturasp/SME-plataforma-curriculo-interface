8966581b70a511a701382306f5ce9ff9f04b548b
Eduardo Omine <eduardo.omine@gmail.com>
2019-01-09 20:03:49 -0200

    implemented searching on pressing enter key

--- src/views/sequences/bar/SearchField.js
+++ src/views/sequences/bar/SearchField.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import TextField from '@material-ui/core/TextField';
 import { connect } from 'react-redux';
+import BodyActions from 'actions/BodyActions';
 import FiltersActions from 'actions/FiltersActions';
+import SequencesActions from 'actions/SequencesActions';
 import iconSearch from 'images/icons/search.svg';
 import styles from './SearchField.scss';
 
@@ -11,6 +13,12 @@ class SearchField extends Component {
 
   onChangedQuery = e => {
     this.props.setQuery(e.target.value);
+  };
+
+  onKeyPress = e => {
+    if (e.key === 'Enter') {
+      this.props.search();
+    }
   };
 
   componentDidMount() {
@@ -31,6 +39,7 @@ class SearchField extends Component {
           classes={{ root: styles.field }}
           label="Buscar sequência por tema"
           onChange={this.onChangedQuery}
+          onKeyPress={this.onKeyPress}
           value={this.state.query}
         />
       </div>
@@ -51,6 +60,10 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
   return {
+    search: value => {
+      dispatch(BodyActions.showLoading());
+      dispatch(SequencesActions.search());
+    },
     setQuery: value => {
       dispatch(FiltersActions.setQuery(value));
     },
