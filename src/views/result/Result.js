f1d694641b257aa5070423c8b47fa673929f8abf
Eduardo Omine <eduardo.omine@gmail.com>
2019-04-09 19:18:03 -0300

    fixed js errors

--- src/views/result/Result.js
+++ src/views/result/Result.js
@@ -49,9 +49,9 @@ class Result extends Component {
           width="100%"
           height="315"
           src={`https://www.youtube.com/embed/${id}`}
-          frameborder="0"
+          frameBorder="0"
           allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-          allowfullscreen
+          allowFullScreen
         />
       );
     });
