7e4687990488931e4f8dd5e0ac617a46e81f7771
Eduardo Omine <eduardo.omine@gmail.com>
2019-05-13 18:59:04 -0300

    Implement formatted file size

--- src/views/result/Attachment.js
+++ src/views/result/Attachment.js
@@ -1,21 +1,25 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { API_URL } from 'data/constants';
+import formatFileSize from 'utils/formatFileSize';
 import iconClip from 'images/icons/clip.svg';
 import styles from './Attachment.scss';
 
 class Attachment extends Component {
   render() {
     const { data } = this.props;
+    const { name, size, url } = data;
     
+    const formattedSize = formatFileSize(size);
+
     return (
-      <a href={API_URL + data.url} className={styles.wrapper} aria-label={data.name} target="_blank">
+      <a href={API_URL + url} className={styles.wrapper} aria-label={name} target="_blank" rel="noopener noreferrer">
         <div className={styles.icon}>
           <img src={iconClip} alt="Anexo" />
         </div>
         <div>
-          <div className={styles.name}>{data.name}</div>
-          <div className={styles.size}>10 MB</div>
+          <div className={styles.name}>{name}</div>
+          <div className={styles.size}>{formattedSize}</div>
         </div>
       </a>
     );
