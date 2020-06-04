45f858a79ddb785f444cfaeef83109e52de90899
rodolphoazeredo <rodolpho.azeredo@amcom.com.br>
2020-06-04 06:29:14 -0300

    Fix: improve.

--- src/views/answerBooks/AnswerBooks.js
+++ src/views/answerBooks/AnswerBooks.js
@@ -60,7 +60,7 @@ class AnswerBooks extends Component {
         })}
       </div>
     );
-    return (sessionStorage.getItem('accessToken') ? <Page>{contents}</Page> : <Page>{contents}</Page>);
+    return (sessionStorage.getItem('accessToken') ? <Page>{contents}</Page> : <Home></Home>);
   }
 };
 export default connect()(AnswerBooks);
