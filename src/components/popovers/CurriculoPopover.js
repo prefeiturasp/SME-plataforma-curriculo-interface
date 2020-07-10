e2d332425951104e730fee1bc862e86a476dee61
rodolphoazeredo <rodolpho.azeredo@amcom.com.br>
2020-07-10 05:17:40 -0300

    Feature: new dropdown.

---
+++ src/components/popovers/CurriculoPopover.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { NavLink } from 'react-router-dom';
+import { connect } from 'react-redux';
+import styles from './ProfilePopover.scss';
+
+class CurriculoPopover extends React.PureComponent {
+  render() {
+    return (
+      <div className={styles.popover} onMouseLeave={this.props.onMouseLeave}>
+        <NavLink to="/curriculo-ensino-fundamental" className={styles.btn}>
+          Ensino Fundamental
+        </NavLink>
+        <NavLink to="/curriculo-ensino-medio" className={styles.btn}>
+          Ensino Médio
+        </NavLink>
+      </div>
+    );
+  }
+}
+
+CurriculoPopover.propTypes = {
+  onMouseLeave: PropTypes.func.isRequired,
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+  };
+};
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(CurriculoPopover);
