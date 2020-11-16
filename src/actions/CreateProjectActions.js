49a6c169eccd3a95541433d722505a8b3a282c4c
rodolphoazeredo <rodolpho.azeredo@amcom.com.br>
2020-11-16 12:01:06 -0300

    Feature: Create Project Actions.

---
+++ src/actions/CreateProjectActions.js
@@ -0,0 +1,75 @@
+import Api from 'data/Api';
+import AlertActions from './AlertActions';
+import { API_URL } from 'data/constants';
+import { history } from 'index';
+import axios from 'axios';
+
+const CreateProjectActions = {
+  LOAD: 'CreateProjectActions.LOAD',
+  LOADED: 'CreateProjectActions.LOADED',
+  LOADED_LEARNING_OBJECTIVES: 'CreateProjectActions.LOADED_LEARNING_OBJECTIVES',
+  LOADED_SCHOOLS: 'CreateProjectActions.LOADED_SCHOOLS',
+
+  load() {
+    return dispatch => {
+      dispatch({type: CreateProjectActions.LOAD })
+      Api.get(dispatch, `/api/project_select_options`)
+        .then(response =>
+          dispatch({ ...response, type: CreateProjectActions.LOADED })
+      )
+    };
+  },
+  getLearningObjectives (curricularComponentIds, yearIds) {
+    let queryString = "";
+    for (let index = 0; index < curricularComponentIds.length; index++) {
+      if (queryString.length < 0) {
+        queryString = `curricular_component_ids[]=${curricularComponentIds[index]}`
+      } else {
+        queryString = `${queryString}&curricular_component_ids[]=${curricularComponentIds[index]}`
+      }
+    }
+    for (let index = 0; index < yearIds.length; index++) {
+      if (queryString.length < 0) {
+        queryString = `year_ids[]=${yearIds[index]}`
+      } else {
+        queryString = `${queryString}&year_ids[]=${yearIds[index]}`
+      }
+    }
+    return dispatch => {
+      Api.get(dispatch, `/api/learning_objectives?${queryString}`)
+        .then(response =>
+          dispatch({ ...response, type: CreateProjectActions.LOADED_LEARNING_OBJECTIVES })
+      )
+    };
+  },
+  getSchools(regionalEducationBoardId) {
+    return dispatch => {
+      Api.get(dispatch, `/api/schools?regional_education_board_id=${regionalEducationBoardId}`)
+        .then(response =>
+          dispatch({ ...response, type: CreateProjectActions.LOADED_SCHOOLS })
+      )
+    };
+  },
+  create(project) {
+    return dispatch => {
+      axios.post(`${API_URL}/api/projects`, project, {
+        headers: {
+          'content-type': 'multipart/form-data'
+        }
+      })
+      .then(res => {
+        dispatch(AlertActions.open(`Projeto salvo com sucesso.`));
+        history.push(`/perfil`);
+        console.log(res.data);
+      })
+      .catch(err => dispatch(AlertActions.open(`Ocorreu um erro: ${err}`)));
+    };
+  },
+  alertError(error) {
+    return dispatch => {
+      return dispatch(AlertActions.open(`Ocorreu um erro: ${error}`))
+    };
+  },
+};
+
+export default CreateProjectActions;
